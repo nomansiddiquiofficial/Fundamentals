0d0623a6b85ece63432a926f19af752845d6cafb
Noman Siddiqui <92144508+nomansiddiquiofficial@users.noreply.github.com>
2024-05-09 11:48:16 +0500

    Add project files.

---
+++ Fundamentals/Program.cs
@@ -0,0 +1,27 @@
+﻿using static System.Runtime.InteropServices.JavaScript.JSType;
+using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        //  ternary operator
+        //ex:1
+        int speed = 100;
+        string status = (speed > 80) ? "Above limit" : "Within limit";
+        Console.WriteLine(status);
+
+        //ex:2
+        string time = (DateTime.Now.Hour < 12) ? "Morning" : "Afternoon";
+        Console.WriteLine(time);
+
+        //ex:3
+        const double pi = 3.14;
+         int number = int.Parse(Console.ReadLine());
+        string result = (number > pi) ? "number is greater" : "number is lesser";
+        Console.WriteLine(result);
+
+        
+    }
+        
+}
