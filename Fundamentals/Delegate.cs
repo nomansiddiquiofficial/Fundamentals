5cf88587bdd8ecbc5297268f6650560fac2836da
Noman Siddiqui <92144508+nomansiddiquiofficial@users.noreply.github.com>
2024-05-10 14:58:08 +0500

    Delegate learning

---
+++ Fundamentals/Delegate.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fundamentals
+{
+    public delegate void Calculation(int a, int b);
+    internal class CustomMath
+    {
+        public static void Addition(int a, int b)
+        {
+            int result = a + b;
+            Console.WriteLine("The result of a and b is:" +result);
+        }
+
+        public static void Divide(int a, int b)
+        {
+            int result = a / b;
+            Console.WriteLine("The result of a and b is: " + result);
+        }
+    }
+}
--- Fundamentals/Program.cs
+++ Fundamentals/Program.cs
@@ -1,27 +1,42 @@
 ﻿using static System.Runtime.InteropServices.JavaScript.JSType;
 using System;
-
-class Program
+namespace Fundamentals
 {
-    static void Main(string[] args)
+
+    class Program
     {
-        //  ternary operator
-        //ex:1
-        int speed = 100;
-        string status = (speed > 80) ? "Above limit" : "Within limit";
-        Console.WriteLine(status);
+        //static void Main(string[] args)
+        //{
+        //    //  ternary operator
+        //    //ex:1
+        //    int speed = 100;
+        //    string status = (speed > 80) ? "Above limit" : "Within limit";
+        //    //Console.WriteLine(status);
 
-        //ex:2
-        string time = (DateTime.Now.Hour < 12) ? "Morning" : "Afternoon";
-        Console.WriteLine(time);
+        //    //ex:2
+        //    string time = (DateTime.Now.Hour < 12) ? "Morning" : "Afternoon";
+        //    //Console.WriteLine(time);
 
-        //ex:3
-        const double pi = 3.14;
-         int number = int.Parse(Console.ReadLine());
-        string result = (number > pi) ? "number is greater" : "number is lesser";
-        Console.WriteLine(result);
+        //    //ex:3
+        //    const double pi = 3.14;
+        //    int number = int.Parse(Console.ReadLine());
+        //    string result = (number > pi) ? "number is greater" : "number is lesser";
+        //    // Console.WriteLine(result);
 
-        
+
+          // Delegates 
+      
+            static void Main(string[] args)
+            {
+                Calculation obj = new Calculation(CustomMath.Addition);
+                obj.Invoke(6, 9); // or obj(6, 9)
+                obj.Invoke(6, 10);
+            }
+       
+
+
+
     }
-        
+
+    
 }
